e6b9d48b894f5d7bac427d35d50f5bead7b038be
Carson Robles <carson.robles@gmail.com>
2026-02-28 11:04:00 -0800

    Add uart driver

--- src/uart/sim/uart_test.py
+++ src/uart/sim/uart_test.py
@@ -1,6 +1,11 @@
+import random
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+
+
+from uart_cocotb import UartTx
 
 
 @cocotb.test()
@@ -9,6 +14,8 @@ async def test_project(dut):
 
     clock = Clock(dut.clk_i, 10, unit="ns")
     cocotb.start_soon(clock.start())
+
+    uart_tx = UartTx(dut.rx_i, baud=115200)
 
     dut._log.info("Reset")
 
@@ -25,21 +32,29 @@ async def test_project(dut):
 
     await ClockCycles(dut.clk_i, 10)
 
-    b = 0x3c
-    b = b | 1 << 8 # put stop bit in upper position
-    b = b << 1     # shift word over 1 for start bit
+    NUM_BYTES = 32
+    data = bytes(random.getrandbits(8) for _ in range(NUM_BYTES))
 
-    for i in range(10):
-        dut.rx_i.value = (b >> i) & 0x1
-        await ClockCycles(dut.clk_i, 5)
-        print(f"dut.rx_data_o = {dut.rx_data_o.value}")
+    print("**************************************************")
+    print("** Test UART Rx                                 **")
+    print("**************************************************")
+    print(f"sending bytes {data}")
+    cocotb.start_soon(uart_tx.send_bytes(data))
+    
+    prev = None
+    recv_cnt = 0
+    while recv_cnt < len(data):
+        await ClockCycles(dut.clk_i, 1)
+        while dut.rx_data_valid_o.value == 0:
+            if prev is None or (dut.rx_data_o.value != prev):
+                #print(f"dut.rx_data_o = {dut.rx_data_o.value}")
+                prev = dut.rx_data_o.value
+            await ClockCycles(dut.clk_i, 1)
 
-    await ClockCycles(dut.clk_i, 1)
-
-    print(f"dut.rx_data_o = {dut.rx_data_o.value}")
-
-    assert dut.rx_data_valid_o.value == 1
-    assert dut.rx_data_o.value == (b >> 1) & 0xff
+        print(f"{recv_cnt+1}/{len(data)}: received byte {hex(dut.rx_data_o.value)}, expecting byte {hex(data[recv_cnt])}")
+        assert dut.rx_data_valid_o.value == 1
+        assert dut.rx_data_o.value == data[recv_cnt] & 0xff
+        recv_cnt += 1
 
     await ClockCycles(dut.clk_i, 1)
 
